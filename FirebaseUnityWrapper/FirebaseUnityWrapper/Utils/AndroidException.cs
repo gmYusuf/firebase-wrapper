300173ed5cf9228c44b42cd84b60da4aa68c9fd6
Yassine Diouri <ametric@gmail.com>
2021-05-19 18:53:31 +0200

    Added initial project and Utils

---
+++ FirebaseUnityWrapper/FirebaseUnityWrapper/Utils/AndroidException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HuaweiMobileServices.Utils
+{
+    using UnityEngine;
+
+    public class AndroidException : Exception
+    {
+
+        public AndroidException(string message) : base(message) { }
+
+        internal AndroidException(int errorCode) : base()
+        {
+            ErrorCode = errorCode;
+        }
+
+        internal AndroidException(AndroidJavaObject javaObject) : base()
+        {
+            JavaException = javaObject;
+        }
+
+        internal AndroidJavaObject JavaException { get; }
+
+        public int ErrorCode { get; }
+
+        public string WrappedExceptionMessage => JavaException.Call<AndroidJavaObject>("getMessage").AsString();
+
+        public string WrappedCauseMessage => JavaException
+            .Call<AndroidJavaObject>("getCause")
+            ?.Call<AndroidJavaObject>("getMessage")
+            .AsString();
+    }
+}
