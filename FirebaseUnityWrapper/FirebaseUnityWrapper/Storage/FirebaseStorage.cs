ba5415886c16149239a81d66ddb5a1b51584ce27
Yassine Diouri <ametric@gmail.com>
2021-09-07 08:50:02 +0200

    Working on Firebase Storage

---
+++ FirebaseUnityWrapper/FirebaseUnityWrapper/Storage/FirebaseStorage.cs
@@ -0,0 +1,21 @@
+﻿using AndroidUtils;
+using UnityEngine;
+
+namespace FirebaseUnityWrapper.Storage
+{
+    public class FirebaseStorage : JavaObjectWrapper
+    {
+        private static readonly AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.google.firebase.storage.FirebaseStorage");
+
+        [UnityEngine.Scripting.Preserve]
+        public FirebaseStorage(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public static FirebaseStorage Instance => androidJavaClass.CallStaticAsWrapper<FirebaseStorage>("getInstance");
+
+        public static FirebaseStorage GetInstance() => androidJavaClass.CallStaticAsWrapper<FirebaseStorage>("getInstance");
+
+        public static FirebaseStorage GetInstance(string url) => androidJavaClass.CallStaticAsWrapper<FirebaseStorage>("getInstance", url);
+
+        public StorageReference Reference => CallAsWrapper<StorageReference>("getReference");
+    }
+}
---
+++ FirebaseUnityWrapper/FirebaseUnityWrapper/Storage/StorageReference.cs
@@ -0,0 +1,30 @@
+﻿using AndroidUtils;
+using GMSTasks;
+using System;
+using UnityEngine;
+
+namespace FirebaseUnityWrapper.Storage
+{
+    public class StorageReference : JavaObjectWrapper
+    {
+
+        [UnityEngine.Scripting.Preserve]
+        public StorageReference(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public StorageReference Child(string path) => CallAsWrapper<StorageReference>("child", path);
+
+        public StorageReference Parent => CallAsWrapper<StorageReference>("getParent");
+
+        public StorageReference Root => CallAsWrapper<StorageReference>("getRoot");
+
+        public ITask<global::AndroidUtils.Void> Delete() => this.CallAsVoidITask("delete");
+
+        public string Bucket => Call<string>("getBucket");
+
+        public ITask<byte[]> GetBytes(long maxDownloadSizeBytes) => this.CallAsByteArrayITask("getBytes", maxDownloadSizeBytes);
+
+        public string Name => Call<string>("getName");
+
+        public string Path => Call<string>("getPath");
+    }
+}
